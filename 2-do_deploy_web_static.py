c0e1ea84ac72bc8079a61ba9d479a63d952824cc
JDMUHIRWA <j.hareriman@alustudent.com>
2024-10-05 17:57:05 +0200

    Refactor do_deploy function for better readability and maintainability

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
 """Fabric Script that distributes an archives"""
 
-
-from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['34.203.198.97', '100.27.191.214']
+from fabric.api import put, run, env
 
+env.hosts = ['34.203.198.97', '100.27.191.214']
 
 def do_deploy(archive_path):
     """Archives to web-servers"""
-    if exists(archive_path) is False:
+    if not exists(archive_path):
         return False
     try:
         file_n = archive_path.split("/")[-1]
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run(f'mkdir -p {path}{no_ext}/')
+        run(f'tar -xzf /tmp/{file_n} -C {path}{no_ext}/')
+        run(f'rm /tmp/{file_n}')
+        run(f'mv {path}{no_ext}/web_static/* {path}{no_ext}/')
+        run(f'rm -rf {path}{no_ext}/web_static')
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run(f'ln -s {path}{no_ext}/ /data/web_static/current')
         return True
-    except:
+    except Exception as e:
         return False
